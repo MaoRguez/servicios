41b3215217a281b59cfae7af5f2ae3f2820b7a49
Mauricio Rodriguez Avila <avila.mauro@gmail.com>
2016-11-30 21:27:07 -0500

    corrección width de las imagenes

--- js/amigo.js
+++ js/amigo.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 		if (scroll > 50) {
 			if (!flag) {
 			$("#logoimg").css({"margin-left": "80px","margin-top": "5px", "width": "50px", "height": "50px"});
-			$("header").css({"background-color": "rgba(97, 97, 97, 0.9)"});
+			$("header").css({"background-color": "rgba(97, 97, 97, 0.9)", "width": "100%"});
 			$(".a").css({"color": "#FFFFFF"});
 			$("#titulo").css({"margin-top": "5px", "color": "#FFFFFF"});
 			flag = true;
@@ -15,7 +15,7 @@ $(document).ready(function(){
 		}else{
 			if (flag) {
 			$("#logoimg").css({"margin-left": "30px","margin-top": "100px", "width": "250px", "height": "250px"});
-			$("header").css({"background-color": "transparent"});
+			$("header").css({"background-color": "transparent", "width": "100%"});
 			$(".a").css({"color": "#607D8B"});
 			$("#titulo").css({"margin-top": "100px", "color": "#607D8B", "font-size": "3em"});
 			flag = false;
