8ee52c45148a10dde82b623d7d852a53e7cd77ec
Mauricio Rodriguez Avila <avila.mauro@gmail.com>
2016-12-06 13:09:27 -0500

    menu responsive con media query

--- js/amigo.js
+++ js/amigo.js
@@ -1,24 +1,34 @@
 $(document).ready(function(){
 	var flag = false;
 	var scroll;
+	var contador = 1;
 		$(window).scroll(function(){
 			scroll = $(window).scrollTop();
 			if (scroll > 50) {
 				if (!flag) {
 				$("#logoimg").css({"margin-left": "80px","margin-top": "5px", "width": "50px", "height": "50px"});
-				$("header").css({"background-color": "rgba(97, 97, 97, 0.9)", "width": "100%"});
-				$(".a").css({"color": "#FFFFFF"});
 				$("#titulo").css({"margin-top": "5px", "color": "#FFFFFF"});
 				flag = true;
 				}
 			}else{
 				if (flag) {
 				$("#logoimg").css({"margin-left": "30px","margin-top": "100px", "width": "250px", "height": "250px"});
-				$("header").css({"background-color": "transparent", "width": "100%"});
-				$(".a").css({"color": "#607D8B"});
 				$("#titulo").css({"margin-top": "100px", "color": "#607D8B", "font-size": "3em"});
 				flag = false;
 				}
+			}
+		});
+		$(".bt-menu480").click(function(){
+			if (contador ==1) {
+				$("#menu").animate({
+					left: "0"
+				});
+				contador = 0;
+			}else{
+				contador = 1;
+				$("#menu").animate({
+					left: "-100%"
+				});
 			}
 		});
 });
